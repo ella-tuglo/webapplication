b23cb175587b6f59b22cba2fc410abfccb02da43
ella-tuglo <ellatuglo@gmail.com>
2022-06-07 15:32:08 +0000

    JS

---
+++ script.js
@@ -0,0 +1,12 @@
+//variables
+//keyword for variable:Let,const, and var
+//var for globbal variable
+
+const guesses= document.querySelector('guesses');
+const lastResult= document.querySelector('lastResult');
+const lowOrHi= document.querySelector('lowOrHi');
+const guessSubmit= document.querySelector('guesseSubmit');
+const guessField= document.querySelector('guessField');
+
+let guessCount=1;
+let guessButton;
